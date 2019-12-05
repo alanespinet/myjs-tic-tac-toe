222c4d2514be3c9a68bbf2a87ce7c3fbf9397b05
Alan Espinet <aelluvetaal@gmail.com>
2019-12-05 18:30:13 -0500

    AI steps 1-2-4

--- src/app.js
+++ src/app.js
@@ -17,46 +17,87 @@
     let completedPlays = 0;
     // alloys the player to keep playing
     let canPlay = true;
+    // possible AI moves
+    let possibleMoves = [];
 
     // forEach is necessary because every single square needs the event. When
     // the click in bound:
         // e.target points to the image inside the .square div (we click the image)
         // this points to the actual .square div.
     document.querySelectorAll('.board .square').forEach( item => {
-        item.addEventListener('click', function(e){
+        item.addEventListener('click', function(e){            
             if( canPlay ){
-                let used = this.getAttribute('used');          
+                let used = this.getAttribute('used'); 
+                let row = this.getAttribute('x');
+                let column = this.getAttribute('y');
+                
                 if( used === '0' ){
-                    e.target.setAttribute('src', './images/' + turn + '.png');
-                    this.setAttribute('used', '1');                
-                    this.setAttribute('value', values[turn]);
-                    let row = this.getAttribute('x');
-                    let column = this.getAttribute('y');
-                    board[row][column] = values[turn];
-                    completedPlays++;
+                    makeMove(row, column, this, e.target);
 
-                    if( gameHasBeenWon(row, column) ){
-                        document.getElementById('result__text').innerText = turn + ' Player Wins!!!';
-                        canPlay = false;
-                    } else {
-                        if( completedPlays === board.length ** 2 ){
-                            document.getElementById('result__text').innerText = 'Well, it\'s a Draw...';
-                            canPlay = false;
-                        }
+                    if( canPlay ){
+                        const aiMove = findPossibleMove();
+                        makeMove( aiMove.x, aiMove.y );
                     }
-
-                    turn = turn === 'cross' ? 'circle' : 'cross';
                 }
             } 
         });
     });
 
-    function isInDirectDiagonal(row, column){
-        return row && parseInt(row) === parseInt(column);
+    function makeMove(row, column, square, img){
+        let squareTag, imgTag;
+        
+        // Get the square and the img related to it
+        if( square && img ){
+            // the player is moving. We are ready to go
+            squareTag = square;
+            imgTag = img;
+        } else {
+            // the computer is moving. We need to search the elements
+            document.querySelectorAll('.board .square').forEach(item => {
+                const x = parseInt(item.getAttribute('x'));
+                const y = parseInt(item.getAttribute('y'));
+
+                if( x === row && y === column ){
+                    squareTag = item;
+                    imgTag = squareTag.children[0];
+                }
+            });
+        }
+
+        // do the actual move
+        imgTag.setAttribute('src', './images/' + turn + '.png');
+        squareTag.setAttribute('used', '1');
+        squareTag.setAttribute('value', values[turn]);
+        board[row][column] = values[turn];
+        completedPlays++;
+        turn = turn === 'cross' ? 'circle' : 'cross';
+
+        // REMOVE
+        // console.log( board );
+        // console.log( sumDirectDiagonalValue() );
+        // console.log( sumInvertedDiagonalValue() );
+        // console.log( '----------------------------' );
+        // REMOVE
+
+        // check if the game is over, with victory...
+        if( gameHasBeenWon(row, column) ){
+            document.getElementById('result__text').innerText = 'We have a winner!!!';
+            canPlay = false;
+        } else {
+            // ...or with draw
+            if( completedPlays === board.length ** 2 ){
+                document.getElementById('result__text').innerText = 'Well, it\'s a Draw...';
+                canPlay = false;
+            }
+        }
+    }
+
+    function isInDirectDiagonal(row, column){        
+        return parseInt(row) === parseInt(column);
     }
 
     function isInInvertedDiagonal(row, column, order){
-        return row && column && parseInt(row) + parseInt(column) === order - 1;
+        return parseInt(row) + parseInt(column) === order - 1;
     }
 
     function gameHasBeenWon(row, column){
@@ -90,7 +131,7 @@
         }
 
         // Direct Diagonal
-        if( isInDirectDiagonal(row, column) && !won ){
+        if( isInDirectDiagonal(row, column) && !won ){            
             // check diagonal only if the win hasn't been achieved
             const diagonalFirstValue = board[0][0];
             let directDiagonalWon = true;
@@ -120,5 +161,165 @@
         } 
 
         return won;
+    }
+
+    function findPossibleMove(){
+        // first the AI will try to win or defend
+        const criticalSpots = findCriticalSpots();
+        // get both arrays
+        const winningSpots = criticalSpots.winningSpots;
+        const defendingSpots = criticalSpots.defendingSpots;
+
+        if( winningSpots.length > 0 ){
+            // we have a winning move, USE it
+            return winningSpots[0];
+        }
+
+        if( defendingSpots.length > 0 ){
+            // we have a defending move, USE it
+            return defendingSpots[0];
+        }
+
+        // random movement, default option
+        // since AI plays every one turn, better to flush the possible moves
+        // to take into consideration last player move
+        possibleMoves = [];
+        const order = board.length;
+
+        for( let i = 0; i < order; i++ )
+            for( let j = 0; j < order; j++ )
+                if( board[i][j] === -1 )
+                    possibleMoves.push({
+                        x: i,
+                        y: j
+                    });
+        
+        // we have an array with all possible moves. Now we simply
+        // peek one random
+        const moveIndex = Math.floor( Math.random() * possibleMoves.length );
+        return possibleMoves[ moveIndex ];
+    }
+
+    function processRowWin(index){
+        let obj = { 
+            sum: 0, 
+            empties: 0,
+            row: -1,
+            column: -1
+        };
+        for( let i = 0; i < board.length; i++ ){
+            obj.sum += board[index][i];
+            if( board[index][i] === -1 ){
+                obj.empties++;
+                obj.row = index;
+                obj.column = i;
+            }
+        }
+        return obj;
+    }
+
+    function processColumnWin(index){
+        let obj = { 
+            sum: 0, 
+            empties: 0,
+            row: -1,
+            column: -1
+        };
+        for( let i = 0; i < board.length; i++ ){
+            obj.sum += board[i][index];
+            if( board[i][index] === -1 ){
+                obj.empties++;
+                obj.row = i;
+                obj.column = index;
+            }
+        }
+        return obj;
+    }
+
+    function processDirectDiagonalWin(){
+        let obj = { 
+            sum: 0, 
+            empties: 0,
+            row: -1,
+            column: -1
+        };
+        for( let i = 0; i < board.length; i++ ){
+            obj.sum += board[i][i];
+            if( board[i][i] === -1 ){
+                obj.empties++;
+                obj.row = i;
+                obj.column = i;
+            }
+        }
+        return obj;
+    }
+
+    function processInvertedDiagonalWin(){
+        let obj = { 
+            sum: 0, 
+            empties: 0,
+            row: -1,
+            column: -1
+        };
+        for( let i = 0, j = board.length - 1; i < board.length; i++, j-- ){
+            obj.sum += board[i][j];
+            if( board[i][j] === -1 ){
+                obj.empties++;
+                obj.row = i;
+                obj.column = j;
+            }
+        }
+        return obj;
+    }
+
+    function findCriticalSpots(){
+        let winningSpots = [];
+        let defendingSpots = [];
+        
+        // first the rows
+        for( let i = 0; i < board.length; i++ ){
+            let possibleRowSpot = processRowWin(i);
+            checkPossibleCriticalSpot(possibleRowSpot, winningSpots, -1);
+            checkPossibleCriticalSpot(possibleRowSpot, defendingSpots, 1);
+        }
+
+        if( winningSpots.length === 0 ){
+            // nothing yet... keep searching. Let's do columns
+            for( let i = 0; i < board.length; i++ ){
+                let possibleColSpot = processColumnWin(i);
+                checkPossibleCriticalSpot(possibleColSpot, winningSpots, -1);
+                checkPossibleCriticalSpot(possibleColSpot, defendingSpots, 1);
+            }
+        }
+
+        if( winningSpots.length === 0 ){
+            // nothing yet... keep searching. Let's do direct diagonal
+            let possibleDDSpot = processDirectDiagonalWin();
+            checkPossibleCriticalSpot(possibleDDSpot, winningSpots, -1);
+            checkPossibleCriticalSpot(possibleDDSpot, defendingSpots, 1);
+        }
+
+        if( winningSpots.length === 0 ){
+            // nothing yet... keep searching. Let's do inverted diagonal
+            let possibleIDSpot = processInvertedDiagonalWin();
+            checkPossibleCriticalSpot(possibleIDSpot, winningSpots, -1);
+            checkPossibleCriticalSpot(possibleIDSpot, defendingSpots, 1);
+        }
+        // return all possible winning and defending spots
+        return {
+            winningSpots,
+            defendingSpots
+        }
+    }
+
+    // process one spot to see if it is viable as a winning one
+    function checkPossibleCriticalSpot(spot, criticalSpotsArray, targetValue){
+        if( spot.sum === targetValue && spot.empties === 1 ){
+            // this is one of the winning spots
+            criticalSpotsArray.push({
+                x: spot.row,
+                y: spot.column
+            });
+        }
     }
 })();
